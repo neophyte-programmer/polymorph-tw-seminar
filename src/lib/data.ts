e55fcf70706a0dd8f24d3358ebf49e850a69c33f
Nutifafa Afi Attor <attorfafa@gmail.com>
2024-11-22 08:04:51 +0000

    feat: add dummy data

---
+++ src/lib/data.ts
@@ -0,0 +1,62 @@
+export const people = [
+    {
+        id: 1,
+        name: "Nutifafa Afi Attor",
+        image: "https://source.unsplash.com/200x200/?portrait,female", // Female portrait placeholder
+        position: "Software Engineer",
+    },
+    {
+        id: 2,
+        name: "Adjoa Mensah",
+        image: "https://source.unsplash.com/200x200/?portrait,woman", // Woman portrait placeholder
+        position: "Product Manager",
+    },
+    {
+        id: 3,
+        name: "Kwame Owusu",
+        image: "https://source.unsplash.com/200x200/?portrait,man", // Male portrait placeholder
+        position: "Data Scientist",
+    },
+    {
+        id: 4,
+        name: "Esi Abena",
+        image: "https://source.unsplash.com/200x200/?portrait,female", // Female portrait placeholder
+        position: "UI/UX Designer",
+    },
+    {
+        id: 5,
+        name: "Kojo Antwi",
+        image: "https://source.unsplash.com/200x200/?portrait,male", // Male portrait placeholder
+        position: "DevOps Engineer",
+    },
+    {
+        id: 6,
+        name: "Ama Serwaa",
+        image: "https://source.unsplash.com/200x200/?portrait,woman", // Woman portrait placeholder
+        position: "Backend Developer",
+    },
+    {
+        id: 7,
+        name: "Yaw Asare",
+        image: "https://source.unsplash.com/200x200/?portrait,man", // Male portrait placeholder
+        position: "Frontend Developer",
+    },
+    {
+        id: 8,
+        name: "Nana Aba",
+        image: "https://source.unsplash.com/200x200/?portrait,female", // Female portrait placeholder
+        position: "Quality Assurance Engineer",
+    },
+    {
+        id: 9,
+        name: "Cynthia Addo",
+        image: "https://source.unsplash.com/200x200/?portrait,woman", // Woman portrait placeholder
+        position: "Scrum Master",
+    },
+    {
+        id: 10,
+        name: "Michael Tetteh",
+        image: "https://source.unsplash.com/200x200/?portrait,male", // Male portrait placeholder
+        position: "Cloud Architect",
+    },
+];
